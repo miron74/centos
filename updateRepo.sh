d689a3760c7d2f691e9324c44dbd0c54e65c89e9
miron74 <55294100+miron74@users.noreply.github.com>
2024-01-13 20:44:29 +0100

    Create updateRepo.sh

---
+++ updateRepo.sh
@@ -0,0 +1,2 @@
+sudo sed -i -e "s|mirrorlist=|#mirrorlist=|g" /etc/yum.repos.d/CentOS-*
+sudo sed -i -e "s|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-*
